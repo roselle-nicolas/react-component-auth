3831df97bc452f063ae6cebeafc7ad246da8147f
Roselle Nicolas <roselle.nicolas.pro@gmail.com>
2021-02-15 14:26:42 +0100

    router + component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-
+import React, { FunctionComponent } from 'react';
 import './App.scss';
+import { BrowserRouter as Router , Link, Route, Switch } from 'react-router-dom';
+import Auth from './pages/auth/auth';
+import HeaderSideBar from './components/header-side-bar/header-side-bar';
 
-function App() {
+const App: FunctionComponent = () => {
 
   return (
     <div className="App">
+      <HeaderSideBar></HeaderSideBar>
+      <Router>
+        <Switch>
+          <Route exact path="/auth" component={Auth} />
+        </Switch>
+      </Router>
       
     </div>
   );
---
+++ src/components/header-side-bar/header-side-bar.tsx
@@ -0,0 +1,33 @@
+import { FunctionComponent } from "react";
+import './header-side-bar.scss';
+
+const slideMenu = () => {
+    const sideNav = document.querySelector('.side-nav');
+    sideNav?.classList.add('side-nav--reveal')
+}
+
+
+const HeaderSideBar: FunctionComponent = () => {
+    return(
+        <nav>
+            <div className="nav-wrapper">
+            <a href="#!" className="brand-logo">Logo</a>
+            <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons" onClick={slideMenu}>menu</i></a>
+            <ul className="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
+                <li><a href="mobile.html">Mobile</a></li>
+            </ul>
+            <ul className="side-nav" id="mobile-demo">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
+                <li><a href="mobile.html">Mobile</a></li>
+            </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default HeaderSideBar;

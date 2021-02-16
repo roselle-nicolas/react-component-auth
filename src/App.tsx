e3cb941bf405b0528ff60606be5b30e3986d2a0c
Roselle Nicolas <roselle.nicolas.pro@gmail.com>
2021-02-16 13:18:49 +0100

    fermeture du menu déroulant

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, MouseEvent } from 'react';
 import './App.scss';
 import { BrowserRouter as Router , Link, Route, Switch } from 'react-router-dom';
 import Auth from './pages/auth/auth';
-import HeaderSideBar from './components/header-side-bar/header-side-bar';
 
 const App: FunctionComponent = () => {
 
+  const handleClick = (event: MouseEvent) => {
+    
+    const sideNav = document.querySelector('.side-nav');
+
+    const isClickOnSideNav = (event.target as Element).classList.contains('side-nav');
+    const isClickOnbuttonCommapse = (event.target as Element).classList.contains('icon-menu');
+    console.log(isClickOnbuttonCommapse);
+    
+    if (!isClickOnSideNav) {
+      if (!isClickOnbuttonCommapse && !isClickOnbuttonCommapse) {
+        sideNav?.classList.remove('side-nav--reveal')
+      }
+    }
+    
+
+  }
+
   return (
-    <div className="App">
-      <HeaderSideBar></HeaderSideBar>
+    <div className="App" onClick={(event) => handleClick(event)}>
       <Router>
         <Switch>
           <Route exact path="/auth" component={Auth} />

c1b35845ec1e489b6f1a3a0dcaafc37d44d4b137
Roselle Nicolas <roselle.nicolas.pro@gmail.com>
2021-02-19 18:02:38 +0100

    nav-bar menu dynamyque

--- src/components/header-side-bar/header-side-bar.tsx
+++ src/components/header-side-bar/header-side-bar.tsx
@@ -3,6 +3,8 @@ import './header-side-bar.scss';
 import HeaderSideBarModel from "./models/header-side-bar";
 //scss
 import '../../sass/utils/_color.scss';
+import menuList from'./menulist';
+import { Link } from "react-router-dom";
 
 const slideMenu = () => {
     const sideNav = document.querySelector('.side-nav');
@@ -23,18 +25,22 @@ const HeaderSideBar: FunctionComponent<Props> = ({headerData}) => {
                 }
                 { headerData.showMenu && 
                     <ul className="right hide-on-med-and-down">
-                        <li><a href="sass.html">Sass</a></li>
-                        <li><a href="badges.html">Components</a></li>
-                        <li><a href="collapsible.html">Javascript</a></li>
-                        <li><a href="mobile.html">Mobile</a></li>
+                        {
+                            menuList.map((menu => (
+                                <li key={menu.name} ><Link to="/"> {menu.name} </Link></li>
+                            )))
+                        }
+                        
+
                     </ul>
                 }
 
                 <ul className="side-nav" id="mobile-demo">
-                    <li><a href="#">Sass</a></li>
-                    <li><a href="#">Components</a></li>
-                    <li><a href="#">Javascript</a></li>
-                    <li><a href="#">Mobile</a></li>
+                    {
+                        menuList.map((menu => (
+                            <li key={menu.name} ><Link to={"/" + menu.link}> {menu.name} </Link></li>
+                        )))
+                    }
                 </ul>
             </div>
         </nav>
---
+++ src/components/header-side-bar/menulist.ts
@@ -0,0 +1,21 @@
+type MenuType = {
+    name: string;
+    link: string;
+}
+
+const menuList: MenuType[] = [
+    {
+        name: "menu1",
+        link: ""
+    },
+    {
+        name: "menu2",
+        link: ""
+    },
+    {
+        name: "menu3",
+        link: ""
+    }
+]
+
+export default menuList;

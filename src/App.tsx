e99183943ce8f9d4f2941650cb6873baf65fbe84
Roselle Nicolas <roselle.nicolas.pro@gmail.com>
2021-02-24 16:47:28 +0100

    reformating

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import Auth from './pages/auth/auth';
 
 const App: FunctionComponent = () => {
 
-  //detection du click dans l'aplication (pour fermet le menu déroulant par exemple)
+  //detection du click dans l'aplication (pour fermer le menu déroulant par exemple)
   const handleClick = (event: MouseEvent) => {
     
     const sideNav = document.querySelector('.side-nav');
 
     const isClickOnSideNav = (event.target as Element).classList.contains('side-nav');
     const isClickOnbuttonCommapse = (event.target as Element).classList.contains('icon-menu');
-    console.log(isClickOnbuttonCommapse);
     
     if (!isClickOnSideNav) {
+      //a vérifier !!!
       if (!isClickOnbuttonCommapse && !isClickOnbuttonCommapse) {
         sideNav?.classList.remove('side-nav--reveal')
       }
